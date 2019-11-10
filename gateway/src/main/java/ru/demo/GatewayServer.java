f511d447f0c8ee18a1dac8de68fba7e20af1006b
Tolkachev Kirill <lavcraft@gmail.com>
2019-11-11 00:09:36 +0300

    add gateway

---
+++ gateway/src/main/java/ru/demo/GatewayServer.java
@@ -0,0 +1,11 @@
+package ru.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServer {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServer.class, args);
+    }
+}
